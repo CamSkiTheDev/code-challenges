50965d1233273f1c77123838105ed5b1383ea46a
Cameron Lucas <cameron.lucas@ibotta.com>
2025-09-17 16:30:17 -0600

    repo clean-up

--- Python/ConvertAndReverseIt.py
+++
@@ -1,8 +0,0 @@
-# https://www.codewars.com/kata/5583090cbe83f4fd8c000051/
-
-def digitize(n):
-    digits = [int(i) for i in list(str(n))]
-    digits.reverse()
-    return digits
-
-print(digitize(35231))
